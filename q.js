06ae62d3bd8655ef57b21a2f5ce28db00ed5a7bd
jgregoryjones <jgregoryjones@users.noreply.github.com>
2020-05-12 13:21:55 -0500

    Update q.js

--- q.js
+++ q.js
@@ -2,13 +2,13 @@ var quotes=new Array()
 
 //change the quotes if desired. Add/ delete additional quotes as desired.
 
-quotes[1]=‘Don\'t wait for your ship to come in, swim out to it.</br> <i>~Cathy Hopkins</i>'
+quotes[1]=‘Don\'t wait for your ship to come in, swim out to it.<i>~Cathy Hopkins</i>'
 
-quotes[2]=‘Don\'t wait for your ship to come in, swim out to it.</br> <i>~Cathy Hopkins</i>'
+quotes[2]=‘Don\'t wait for your ship to come in, swim out to it.~Cathy Hopkins</i>'
 
-quotes[3]=‘Don\'t wait for your ship to come in, swim out to it.</br> <i><b>~Cathy Hopkins</b></i>'
+quotes[3]=‘Don\'t wait for your ship to come in, swim out to it.<i><b>~Cathy Hopkins</b></i>'
 
-quotes[4]=‘Don\'t wait for your ship to come in, swim out to it.</br> <i>~Bob</i>'
+quotes[4]=‘Don\'t wait for your ship to come in, swim out to it.<i>~Bob</i>'
 
 var whichquote=Math.floor(Math.random()*(quotes.length))
 document.write(quotes[whichquote])
