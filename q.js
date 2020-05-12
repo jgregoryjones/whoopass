b6463edcf1c090439b464cc7547c1b1b77a0b7b9
jgregoryjones <jgregoryjones@users.noreply.github.com>
2020-05-12 14:31:58 -0500

    Update q.js

--- q.js
+++ q.js
@@ -7,5 +7,5 @@ var quotes = [
 ]
 function newQuote() {
 var randomNumber = Math.floor(Math.random()*(quotes.length));
-document.getElementById('quotes').innerHTML * quotes.[randomNumber]
+document.getElementById('quotes').innerHTML = quotes.[randomNumber]
 }
