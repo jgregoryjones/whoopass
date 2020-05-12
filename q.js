7d55885483f3da3aa943b055331df47c2d0b48a2
jgregoryjones <jgregoryjones@users.noreply.github.com>
2020-05-12 14:33:30 -0500

    Update q.js

--- q.js
+++ q.js
@@ -1,9 +1,9 @@
 //change the quotes if desired. Add or delete additional quotes as desired.
 var quotes = [
-‘Don\'t wait for your ship to come in, swim out to it.<i>~Cathy Hopkins</i>',
-‘Don\'t wait for your ship to come in, swim out to it.~Cathy Hopkins</i>',
-‘Don\'t wait for your ship to come in, swim out to it.<i><b>~Cathy Hopkins</b></i>',
-‘Don\'t wait for your ship to come in, swim out to it.<i>~Bob</i>'
+‘Don\'t wait for your ship to come in, swim out to it.',
+‘Don\'t wait for your ship to come in, swim out to it.',
+‘Don\'t wait for your ship to come in, swim out to it.',
+‘Don\'t wait for your ship to come in, swim out to it.~bob'
 ]
 function newQuote() {
 var randomNumber = Math.floor(Math.random()*(quotes.length));
