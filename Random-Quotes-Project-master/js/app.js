660692c070d028f3c98afdf9b4cdd498d3c60033
jgregoryjones <jgregoryjones@users.noreply.github.com>
2020-05-13 16:05:00 -0500

    Update app.js

--- Random-Quotes-Project-master/js/app.js
+++ Random-Quotes-Project-master/js/app.js
@@ -3,44 +3,33 @@
 const quotes = [
     {
       quote:
-        "Most men lead lives of quiet desperation and go to the grave with their song still in them.",
-      author: "Henry David Thoreau"
+        "Life",
+      author: "Life"
     },
     {
       quote:
-        "God save me from my friends. I can protect myself from my enemies.",
+        "Humor",
+      author: "John Kenneth Galbraith"
+    },
+    {
+      quote:
+        "God.",
       author: "Claude Louis Hector de Villars"
     },
     {
-      quote: "The price of anything is the amount of life you exchange for it.",
-      author: "Henry David Thoreau"
+      quote: "The",
+      author: "HENRY David Thoreau"
     },
     {
       quote:
-        "Bullshit is work that is done merely to complete requirements, make you look good, or otherwise fill up the hours of the day.",
-      author: "Chris Guillebeau, The Art of Non-Conformity"
+        "xxx is",
+      author: "Charles Lindbergh"
     },
     {
       quote:
-        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
+        "Axxxx critic",
       author: "Tyne Daly"
     }
-{
-      quote:
-        "You don’t have to live your life the way other people expect you to.",
-      author: "Chris Guillebeau, The Art of Non-Conformity"
-    }
-{
-      quote:
-        "I don’t understand why people are frightened of new ideas. I’m frightened of the old ones.",
-      author: "John Cage"
-    }
-{
-      quote:
-        "The great pleasure in life is doing what people say you cannot do.",
-      author: "Walter Bagehot"
-    }
-
   ];
   
   const btn = document.getElementById("generate-btn");
