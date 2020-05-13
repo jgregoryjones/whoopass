3b8a79f7bd474e7a9748ccc2db47222d15dc525c
jgregoryjones <jgregoryjones@users.noreply.github.com>
2020-05-13 15:48:55 -0500

    Update app.js

--- Random-Quotes-Project-master/js/app.js
+++ Random-Quotes-Project-master/js/app.js
@@ -1,37 +1,37 @@
 //immediate invoked function expression
 (function() {
-  const quotes = [
+const quotes = [
     {
       quote:
-        "Life is too short and sweet to be spent by cribbing and complaining about things. Here are some random quotes about the most wonderful gift that we've got",
-      author: " Life"
+        "Life",
+      author: "Life"
     },
     {
       quote:
-        "Humor is richly rewarding to the person who employs it. It has some value in gaining and holding attention. But it has no persuasive value at all",
+        "Humor",
       author: "John Kenneth Galbraith"
     },
     {
       quote:
-        "God save me from my friends. I can protect myself from my enemies.",
-      author: "Claude Louis Hector de Villars "
+        "God.",
+      author: "Claude Louis Hector de Villars"
     },
     {
-      quote: "The price of anything is the amount of life you exchange for it.",
-      author: "David Thoreau"
+      quote: "The",
+      author: "HENRY David Thoreau"
     },
     {
       quote:
-        "Life is like a landscape. You live in the midst of it but can describe it only from the vantage point of distance. ",
+        "Life is",
       author: "Charles Lindbergh"
     },
     {
       quote:
-        "A critic is someone who never actually goes to the battle, yet who afterwards comes out shooting the wounded.",
-      author: " Tyne Daly"
+        "A critic",
+      author: "Tyne Daly"
     }
   ];
-
+  
   const btn = document.getElementById("generate-btn");
 
   btn.addEventListener("click", function() {
